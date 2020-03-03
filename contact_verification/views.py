14edaa2bb5acd037207fc33af655cbd96672fcd8
khj809 <onsealeatang@gmail.com>
2020-03-03 23:11:01 +0900

    set permission to AllowAny for contact verification view and country view

--- contact_verification/views.py
+++ contact_verification/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
 from contact_verification.data import COUNTRY_PHONES
@@ -15,6 +15,7 @@ from contact_verification.serializers import ContactVerificationSerializer, Cont
 class ContactVerificationViewSet(mixins.CreateModelMixin, GenericViewSet):
     queryset = ContactVerification.objects.all()
     serializer_class = ContactVerificationSerializer
+    permission_classes = (AllowAny,)
 
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
@@ -33,6 +34,7 @@ class ContactViewSet(mixins.CreateModelMixin, GenericViewSet):
 
 class CountryViewSet(ReadOnlyModelViewSet):
     serializer_class = CountrySerializer
+    permission_classes = (AllowAny,)
 
     def get_queryset(self):
         return [{'number': country[0], 'name': country[1]} for country in COUNTRY_PHONES]
